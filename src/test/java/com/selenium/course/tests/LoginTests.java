c104a6f3ba7f2486f619371a837855c9861b3e6b
Ibraim Asanov <iasanov@nemetschek.bg>
2022-01-28 19:03:05 +0200

    Fix issues

--- src/test/java/com/selenium/course/tests/LoginTests.java
+++ src/test/java/com/selenium/course/tests/LoginTests.java
@@ -28,7 +28,7 @@ public class LoginTests extends TestUtil {
     }
 
         @Test (dataProvider = "usersCsv") //This cames from TestNG
-        public void unsuccessfulLogin(String userName, String password){
+        public void unsuccessfulLogin(String userName, String password) throws InterruptedException {
 
             WebElement userNameInput = driver.findElement(By.id("user-name"));
             userNameInput.sendKeys(userName);
@@ -42,5 +42,6 @@ public class LoginTests extends TestUtil {
             WebElement wrongUserBtn = driver.findElement(By.cssSelector(".error-button"));
 
             Assert.assertTrue(wrongUserBtn.isDisplayed());
+            Thread.sleep(5000);
         }
     }
--- src/test/java/com/selenium/course/tests/ProductTests.java
+++ src/test/java/com/selenium/course/tests/ProductTests.java
@@ -7,7 +7,27 @@ import org.testng.annotations.Test;
 public class ProductTests extends TestUtil {
 
     @Test
-    public void addItemToTheCart(){
+    public void addItemToTheCart() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(driver);
+        ProductsPage productPage = loginPage.login("standard_user","secret_sauce");
+        productPage.addToCartByProductName("backpack");
+        Thread.sleep(5000);
+
+        Assert.assertEquals(productPage.getNumbersInTheCart(), 1, "Because we have added one item in the cart.");
+    }
+
+    @Test
+    public void addItemToTheCart1()throws InterruptedException{
+        LoginPage loginPage = new LoginPage(driver);
+        ProductsPage productPage = loginPage.login("standard_user","secret_sauce");
+        productPage.addToCartByProductName("backpack");
+        Thread.sleep(5000);
+
+        Assert.assertEquals(productPage.getNumbersInTheCart(), 1, "Because we have added one item in the cart.");
+    }
+
+    @Test
+    public void addItemToTheCart2(){
         LoginPage loginPage = new LoginPage(driver);
         ProductsPage productPage = loginPage.login("standard_user","secret_sauce");
         productPage.addToCartByProductName("backpack");
